a15c62171d65bb5f1c7b334f6e8cf6de29b717f7
Eric Burkheimer <eaburk@outlook.com>
2025-12-17 09:52:39 -0600

    initial commit

---
+++ db.js
@@ -0,0 +1,29 @@
+const Database = require('better-sqlite3');
+const path = require('path');
+
+// Connect to the local file (it will be created if it doesn't exist)
+const db = new Database(path.join(__dirname, 'database.db'), {
+  verbose: console.log // Logs all SQL queries to your terminal
+});
+
+// Create tables if they don't exist
+db.exec(`
+  CREATE TABLE IF NOT EXISTS companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
+
+  CREATE TABLE IF NOT EXISTS time_entries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    statDate DATETIME,
+    endDate DATETIME,
+    billed BOOLEAN DEFAULT false,
+    manual_entry BOOLEAN DEFAULT false,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
+`);
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/companies', (req, res) => {
+  try {
+    const rows = db.prepare('SELECT * FROM companies').all();
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get('/api/time-entries', (req, res) => {
+  const { startDate, endDate } = req.query;
+
+  try {
+    //const rows = db.prepare('SELECT * FROM time_entries').all();
+    const stmt = db.prepare('SELECT * FROM time_entries WHERE DATE(startDate) >= ? AND DATE(startDate) <= ?');
+    rows = stmt.all(startDate, endDate);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST route to create a time entry
+app.post('/api/time-entries', (req, res) => {
+  const { startDate, endDate } = req.body;
+  console.log(startDate, endDate);
+
+  if (!startDate || !endDate) {
+    return res.status(400).json({ error: "Missing time_in or time_out" });
+  }
+
+  try {
+    const stmt = db.prepare('INSERT INTO time_entries (startDate, endDate) VALUES (?, ?)');
+    const info = stmt.run(startDate, endDate);
+
+    // Return the newly created record
+    res.status(201).json({
+      id: info.lastInsertRowid,
+      startDate,
+      endDate
+    });
+  } catch (err) {
+    console.error("Database Error:", err.message);
+    res.status(500).json({ error: "Failed to save record" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
